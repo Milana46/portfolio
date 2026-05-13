f990cb6f53d9f4afa6730e508718fe20b4e191ff
Milana46 <bogatyrevamilana462004@gmail.com>
2026-05-13 16:05:25 +0300

    fix: base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
+/// <reference types="node" />
 // @ts-check
 import { defineConfig } from 'astro/config';
 
 const repository = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
 const owner = process.env.GITHUB_REPOSITORY?.split("/")[0] ?? "your-username";
 const site = `https://${owner}.github.io`;
-const base = repository ? `/${repository}` : "/";
+const base = repository ? `/${repository}/` : "/";
 
 // https://astro.build/config
 export default defineConfig({
