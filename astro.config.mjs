5860fddf5b03af06107f8b5bd5c5fcbf2017dd61
Milana46 <bogatyrevamilana462004@gmail.com>
2026-05-13 08:57:39 +0300

    Remove custom domain configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import { defineConfig } from 'astro/config';
 
 const repository = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
 const owner = process.env.GITHUB_REPOSITORY?.split("/")[0] ?? "your-username";
-const customDomain = process.env.CUSTOM_DOMAIN;
-const site = customDomain ? `https://${customDomain}` : `https://${owner}.github.io`;
-const base = customDomain ? "/" : repository ? `/${repository}` : "/";
+const site = `https://${owner}.github.io`;
+const base = repository ? `/${repository}` : "/";
 
 // https://astro.build/config
 export default defineConfig({
